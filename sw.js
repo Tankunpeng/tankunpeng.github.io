2fe61b56cf377bdf2eca210f9723fd26165ac875
Tankunpeng <tankunpeng@live.cn>
2019-07-28 21:38:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,3 +2,40 @@ self.addEventListener('install', function(event) {
     // Perform install steps
     console.log(13221312)
 });
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/styles/main.css',
+    '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    console.log(13221312)
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
