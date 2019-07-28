fe2dddfcfbcb78f9e71b659c218636271f34c876
Tankunpeng <tankunpeng@live.cn>
2019-07-28 21:50:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -27,6 +27,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+    console.log('fetch', event.request);
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
